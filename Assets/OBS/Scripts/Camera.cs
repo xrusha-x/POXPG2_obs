42c324562e5a04565b006eacec562b29c8d1fef3
xrusha-x <s30878@pjwstk.edu.pl>
2025-11-25 15:09:42 +0100

    camera
    5 funk camera

---
+++ Assets/OBS/Scripts/Camera.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public enum CameraMode
+    {
+        SimpleFollow,
+        SmoothFollow,
+        DeadZone,
+        LookAhead
+    }
+
+    public CameraMode mode = CameraMode.SimpleFollow;
+    public Transform player;
+    public Vector3 offset = new Vector3(0, 5, -10);
+    public float smoothTime = 0.3f;
+    public Vector2 deadZone = new Vector2(1f, 1f);
+    public float lookAheadDistance = 2f;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 lastPlayerPos;
+
+    void Start()
+    {
+        if (player != null)
+            lastPlayerPos = player.position;
+    }
+
+    void LateUpdate()
+    {
+        if (player == null) return;
+
+        switch (mode)
+        {
+            case CameraMode.SimpleFollow:
+                transform.position = player.position + offset;
+                break;
+
+            case CameraMode.SmoothFollow:
+                transform.position = Vector3.SmoothDamp(transform.position, player.position + offset, ref velocity, smoothTime);
+                break;
+
+            case CameraMode.DeadZone:
+                Vector3 newPos = transform.position;
+                if (Mathf.Abs(player.position.x - transform.position.x) > deadZone.x)
+                    newPos.x = player.position.x - Mathf.Sign(player.position.x - transform.position.x) * deadZone.x;
+                if (Mathf.Abs(player.position.y - transform.position.y) > deadZone.y)
+                    newPos.y = player.position.y - Mathf.Sign(player.position.y - transform.position.y) * deadZone.y;
+                transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
+                break;
+
+            case CameraMode.LookAhead:
+                Vector3 moveDelta = player.position - lastPlayerPos;
+                Vector3 lookAhead = new Vector3(moveDelta.x, moveDelta.y, 0) * lookAheadDistance;
+                transform.position = Vector3.SmoothDamp(transform.position, player.position + offset + lookAhead, ref velocity, smoothTime);
+                lastPlayerPos = player.position;
+                break;
+        }
+    }
+}
